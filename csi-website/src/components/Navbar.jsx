b16ebe736ea5eb68e0f614c14eb05b96efb85a43
Mayank <mayank.kulkarni29@gmail.com>
2024-08-15 11:45:58 +0530

    alignment adjustment in navbar

--- csi-website/src/components/Navbar.jsx
+++ csi-website/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gradient-to-b from-black to-transparent p-4 fixed w-full top-0 left-0 z-10">
-      <div className="container mx-auto flex justify-between items-center px-4 lg:px-8">      
+      <div className="container mx-auto flex justify-between items-center px-4 lg:px-8">
         <div className="flex items-center space-x-2 lg:space-x-4">
           <img src={logo} alt="CSI VIT Pune" className="h-8 sm:h-10 mr-2 sm:mr-4" />
           <div className="text-white whitespace-nowrap text-sm sm:text-base lg:text-xl">
@@ -25,15 +25,15 @@ const Navbar = () => {
             {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
-        <ul className={`lg:flex lg:items-center lg:justify-end font-bold font-inter space-x-12 ${isOpen ? 'absolute top-full left-0 w-full bg-black text-white p-4 rounded-lg flex flex-col items-center' : 'hidden'} lg:static lg:flex-row lg:space-x-12 lg:mr-8`}>
+        <ul className={`lg:flex lg:items-center lg:justify-end font-bold font-inter space-x-12 ${isOpen ? 'absolute top-full right-0 w-64 bg-black text-white p-4 rounded-lg flex flex-col items-center' : 'hidden'} lg:static lg:flex-row lg:space-x-12 lg:mr-8`}>
           <li className="mt-4 lg:mt-0">
-            <a href="#events" className="text-white hover:text-pink-700 transition duration-300 ml-14 md:ml-0">Events</a>
+            <a href="#events" className="text-white hover:text-pink-700 transition duration-300">Events</a>
           </li>
           <li className="mt-4 lg:mt-0">
-            <a href="#about" className="text-white hover:text-pink-700 transition duration-300">About</a>
+            <a href="#about" className="text-white hover:text-pink-700 transition duration-300 mr-12 md:mr-0">About</a>
           </li>
           <li className="mt-4 lg:mt-0">
-            <a href="#team" className="text-white hover:text-pink-700 transition duration-300">Team</a>
+            <a href="#team" className="text-white hover:text-pink-700 transition duration-300 mr-12 md:mr-0">Team</a>
           </li>
         </ul>
       </div>
